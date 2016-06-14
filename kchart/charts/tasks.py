4af2be434340385c29d5ef440d877a62ed696567
Peter Rowlands <peter@pmrowla.com>
2016-06-15 03:20:55 +0900

    add missing tasks file

---
+++ kchart/charts/tasks.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+from celery import shared_task
+
+from .utils import MelonChartService
+
+
+@shared_task
+def update_melon_hourly_chart():
+    melon = MelonChartService()
+    melon.fetch_hourly()
