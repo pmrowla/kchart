38d48266998c96286f8a9edf2cd3c5dd0d857cc9
Peter Rowlands <peter@pmrowla.com>
2016-06-17 01:17:45 +0900

    register music services to the admin page

--- kchart/charts/admin.py
+++ kchart/charts/admin.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+
+from django.contrib import admin
+
+from .models import (
+    MusicService,
+)
+
+
+admin.site.register(MusicService)
