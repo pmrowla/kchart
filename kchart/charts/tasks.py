849820baf068771ed937c9dc30f81fda66d76f0c
Peter Rowlands <peter@pmrowla.com>
2016-06-19 21:46:56 +0900

    [celery] add backlog task, retry fetch tasks on connection error

--- kchart/charts/tasks.py
+++ kchart/charts/tasks.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
+from datetime import timedelta
+
 from celery import shared_task, chord
+from requests.exceptions import ConnectionError, ConnectTimeout
 
 from .chartservice import (
     BugsChartService,
@@ -10,7 +13,7 @@ from .chartservice import (
     MnetChartService,
 )
 from .models import AggregateHourlySongChart
-from .utils import utcnow
+from .utils import utcnow, strip_to_hour
 
 
 @shared_task
@@ -19,21 +22,30 @@ def aggregate_hourly_chart(hour=utcnow()):
 
 
 @shared_task
-def update_bugs_hourly_chart(hour=utcnow()):
+def update_bugs_hourly_chart(hour=utcnow(), **kwargs):
     bugs = BugsChartService()
-    return bugs.fetch_hourly(hour)
+    try:
+        return bugs.fetch_hourly(hour)
+    except (ConnectionError, ConnectTimeout) as exc:
+        raise update_bugs_hourly_chart.retry(hour=hour, exc=exc, kwargs=kwargs)
 
 
 @shared_task
-def update_mnet_hourly_chart(hour=utcnow()):
+def update_mnet_hourly_chart(hour=utcnow(), **kwargs):
     mnet = MnetChartService()
-    return mnet.fetch_hourly(hour)
+    try:
+        return mnet.fetch_hourly(hour)
+    except (ConnectionError, ConnectTimeout) as exc:
+        raise update_mnet_hourly_chart.retry(hour=hour, exc=exc, kwargs=kwargs)
 
 
 @shared_task
-def update_genie_hourly_chart(hour=utcnow()):
+def update_genie_hourly_chart(hour=utcnow(), **kwargs):
     genie = GenieChartService()
-    return genie.fetch_hourly(hour)
+    try:
+        return genie.fetch_hourly(hour)
+    except (ConnectionError, ConnectTimeout) as exc:
+        raise update_genie_hourly_chart.retry(hour=hour, exc=exc, kwargs=kwargs)
 
 
 @shared_task
@@ -51,3 +63,21 @@ def update_melon_hourly_chart():
     result = melon.fetch_hourly()
     update_dependent_hourly_charts.delay(result.hour)
     return result
+
+
+@shared_task
+def backlog_hourly_range(start_hour=utcnow(), delta=timedelta(hours=23)):
+    '''Fill hourly chart backlog, working backwards from [start_hour, (start_hour - delta)]
+
+    :param datetime start_hour: timestamp from which to begin the backlog operation
+    :param timedelta delta: the amount of time to backlog
+
+    Chart updates will be spaced at least 30 seconds apart to avoid spamming requests
+    '''
+    hour = strip_to_hour(start_hour)
+    end_hour = start_hour - delta
+    countdown = 30
+    while hour >= end_hour:
+        update_dependent_hourly_charts.apply_async((hour,), countdown=countdown)
+        countdown += 30
+        hour = hour - timedelta(hours=1)
