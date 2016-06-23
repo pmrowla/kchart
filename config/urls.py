87baafdebab8d76d7ba84fb2820896e276d8f349
Peter Rowlands <peter@pmrowla.com>
2016-06-23 23:04:07 +0900

    disable user views for now

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     url(settings.ADMIN_URL, include(admin.site.urls)),
 
     # User management
-    url(r'^users/', include('kchart.users.urls', namespace='users')),
-    url(r'^accounts/', include('allauth.urls')),
+    # url(r'^users/', include('kchart.users.urls', namespace='users')),
+    # url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
     url(r'^api/v1/', include('kchart.api.urls', namespace='api-v1')),
