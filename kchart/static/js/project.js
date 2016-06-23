e3680d75f5c72e33714f1adb27153e80db3c419d
Peter Rowlands <peter@pmrowla.com>
2016-06-23 16:46:03 +0900

    add song info modal to charts page

--- kchart/static/js/project.js
+++ kchart/static/js/project.js
@@ -1,3 +1,39 @@
 /* Project specific Javascript goes here. */
 $( document ).ready(function() {
 });
+
+$('#songModal').on('show.bs.modal', function(event) {
+  var button = $(event.relatedTarget);
+  var songId = button.data('songId');
+  var chartSlug = button.data('chart-slug');
+  var modal = $(this);
+
+  var url = '/api/v1/songs/' + songId + '/?format=json';
+
+  $.getJSON(url, function(data) {
+    var artistNames = [];
+
+    data['artists'].forEach(function(artist) {
+      artistNames.push(artist['name']);
+    });
+
+    var currentRank = 'Unranked';
+    var peakRank = 'Unranked';
+
+    var kchartDetails = data['chart_details']['realtime']['kchart'];
+    if (kchartDetails['has_charted'] == true) {
+      if (kchartDetails['current_position'])
+      {
+        currentRank = kchartDetails['current_position'];
+      }
+      peakRank = kchartDetails['peak_position'];
+    }
+
+    modal.find('#songTitle').text(data['name']);
+    modal.find('#songAlbum').text(data['album']['name']);
+    modal.find('#songArtists').text(artistNames.join(', '));
+    modal.find('#songReleaseDate').text(data['release_date']);
+    modal.find('#songRank').text(currentRank);
+    modal.find('#songPeakRank').text(peakRank);
+  });
+});
